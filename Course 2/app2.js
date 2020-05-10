fbf5c67248dccad057b43e9ada20bb1ea5d31862
Ricardo Ruiz <rrcfesc@gmail.com>
2020-05-10 01:43:50 -0500

    WIP

--- Course 2/app2.js
+++ Course 2/app2.js
@@ -1,22 +1,39 @@
-Vue.component('plan-picker', {
-  template: '#plan-picker-template',
-  data() {
-    return {
-      plans: ['The Single', 'The Curious', 'The Addict']
-    }
-  }
-})
-
-Vue.component('plan', {
+let PlanComponent = {
   template: '#plan-template',
   props: {
     name: {
       type: String,
       required: true
     }
+  },
+  data() {
+    return {
+      selected: false
+    }
+  },
+  methods: {
+    select() {
+      this.$emit('hi')
+      this.selected = true
+    }
   }
-})
+}
+ 
+let PlanPickerComponent =  {
+  template: '#plan-picker-template',
+  components: {
+    plan: PlanComponent
+  },
+  data() {
+    return {
+      plans: ['The Single', 'The Curious', 'The Addict']
+    }
+  }
+}
 
 new Vue({
-  el: '#app'
+  el: '#app',
+  components: {
+    'plan-picker': PlanPickerComponent
+  }
 })
